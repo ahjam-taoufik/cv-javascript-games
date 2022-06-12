be8fc5261c3cd8b6cc05a710cc33206bf697b1b0
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-12 17:55:35 +0200

    add jump

---
+++ js/DinoGame.js
@@ -0,0 +1,10 @@
+
+const character=document.getElementById("character");
+const block=document.getElementById("block");
+
+function jump(){
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
+}
